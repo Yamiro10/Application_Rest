f4ae8b710df1151b3fec8adee572a66b5643eb42
Yamiro10 <Ciaoydr@gmail.com>
2021-05-07 17:54:34 +0200

    Initial commit

---
+++ src/main/java/com/example/Application_Rest/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.Application_Rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
